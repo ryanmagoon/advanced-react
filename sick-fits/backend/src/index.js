4eaa84faa95bd136a411d755bed6f02ec4a9145d
Ryan Magoon <ryanmagoon@pop-os.localdomain>
2018-10-28 02:07:49 -0400

    add password support to server

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,10 +1,13 @@
+const cookieParser = require('cookie-parser')
+
 require('dotenv').config({ path: 'variables.env' })
 const createServer = require('./createServer')
 const db = require('./db')
 
 const server = createServer()
 
-// TODO use express to handle cookies (JWT)
+server.express.use(cookieParser())
+
 // TODO use express to populate current user
 
 server.start(
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const mutations = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem(
@@ -36,6 +39,26 @@ const mutations = {
       }`
     )
     return ctx.db.mutation.deleteItem({ where }, info)
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase()
+    const password = await bcrypt.hash(args.password, 10)
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      },
+      info
+    )
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    })
+    return user
   }
 }
 
