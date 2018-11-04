5e4ef4801b06f10bd23aea2b7afe61deb9f455fa
Ryan Magoon <mr.magoon5@gmail.com>
2018-11-04 14:06:39 -0500

    add front end support for currently logged in user

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,8 +1,15 @@
 import Link from 'next/link'
 import NavStyles from './styles/NavStyles'
+import User from './User'
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {({ data: { me } }) => {
+        console.log(me)
+        return me ? <p>{me.name}</p> : null
+      }}
+    </User>
     <Link href="/items">
       <a>Shop</a>
     </Link>
